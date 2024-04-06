d0f98000d4af2f0ac76b9929b1e31a99652e5627
abdullahayhan <aayhaan4@gmail.com>
2024-04-07 02:38:27 +0300

    Validation Pipeline Behaviour is impelemented.

---
+++ Application/Exceptions/CustomValidationException.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Exceptions;
+
+public class CustomValidationException : Exception
+{
+    public List<string> ErrorsMessage { get; set; } = new();
+    public string? FriendlyErrorsMessage { get; set; }
+
+    public CustomValidationException(List<string> errorsMessage, string? friendlyErrorsMessage)
+        : base(friendlyErrorsMessage)
+    {
+        ErrorsMessage = errorsMessage;
+        FriendlyErrorsMessage = friendlyErrorsMessage;
+    }
+}
---
+++ Application/Pipelines/ValidationPipelineBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using Application.Exceptions;
+using FluentValidation;
+using MediatR;
+
+namespace Application.Pipelines;
+
+public class ValidationPipelineBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationPipelineBehaviour(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, 
+        RequestHandlerDelegate<TResponse> next, 
+        CancellationToken cancellationToken)
+    {
+        if (_validators.Any())
+        {
+            var context = new ValidationContext<TRequest>(request);
+            List<string> errors = new();
+
+            var validationResult = await Task
+                .WhenAll(_validators
+                    .Select(validationResult => validationResult.ValidateAsync(context,
+                    cancellationToken)));
+
+            var failures = validationResult.SelectMany(vr => vr.Errors)
+                .Where(failure => failure != null)
+                .ToList();
+
+            foreach (var failure in failures)
+            {
+                errors.Add(failure.ErrorMessage);
+            }
+
+            throw new CustomValidationException(errors,"One or more validation failure(s) occured."); // yalan geceler.
+        }
+
+        return await next();
+    }
+}
